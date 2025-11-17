78b4d44fe6a11cc9a30e2bedeae9cd9144457fd2
ewenhrg <ewenkermorvantpro@gmail.com>
2025-11-17 10:45:49 +0200

    Revert: Suppression du placeholder dans le champ tÃ©lÃ©phone

--- src/pages/RequestPage.jsx
+++ src/pages/RequestPage.jsx
@@ -538,7 +538,6 @@ export function RequestPage() {
                     onChange={(e) =>
                       setFormData({ ...formData, clientPhone: e.target.value })
                     }
-                    placeholder="Ex: 33607070707"
                     aria-required="true"
                     aria-describedby="tooltip-phone"
                     className="text-base sm:text-lg py-3 sm:py-3.5 border-2 border-gray-200 focus:border-blue-500 focus:ring-4 focus:ring-blue-100 transition-all duration-300 shadow-sm hover:shadow-md disabled:opacity-50 disabled:cursor-not-allowed touch-manipulation"
